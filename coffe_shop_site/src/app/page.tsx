3960d5bd26aea77467bfec53428f85eb3ff34782
Hamdhi <hamdhiharis@gmail.com>
2025-09-06 01:05:59 +0530

    feat:Product Explorer Added

--- coffe_shop_site/src/app/page.tsx
+++ coffe_shop_site/src/app/page.tsx
@@ -1,10 +1,12 @@
 
-
+import ProductExplorer from "./components/Product-Explorer/Product-Explorer";
 export default function Home() {
   return (
     <div>
       <h1>Welcome to Our Coffee Shop</h1>
       <p>We serve the best coffee in town.</p>
+      {/* product-explorer */}
+      <ProductExplorer />
     </div>
   );
 }
