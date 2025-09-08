6df78dab1120370ecbcf9d6b5f541343c3bd1774
UGHarsha <harshalakshitha1@gmail.com>
2025-09-08 17:46:15 +0530

    add home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
+import HomeComponent from  "@/components/Home";
 import ProductExplorer from "@/components/productExplorer";
 import Image from "next/image";
+
 
 export default function Home() {
   return (
    <>
     {/* <ProductExplorer/>   */}
+    <HomeComponent/>
     <ProductExplorer/>
+    
    </>
   );
 }
