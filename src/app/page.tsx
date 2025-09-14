5c07c93661abf5c7fcf13506c2581fb6b1418750
Pasindu86 <pasindupamudith@gmail.com>
2025-09-14 22:22:40 +0530

    fix the order

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,6 +11,8 @@ export default function Home() {
     <About/>
     {/* <ProductExplorer/>   */}
     <HomeComponent/>
+       {/* <About/>   */}
+    <About/>
     <ProductExplorer/>
     
    </>
